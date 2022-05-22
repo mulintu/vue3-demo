e0e550c61ed6f6e6b04c42e727987dbe87b81ad5
tuyaqin1 <806149557@qq.com>
2022-05-22 23:01:58 +0800

    创建项目

---
+++ src/components/todolist/TodoList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <input type="text" v-model="title" @keypress.enter="addTodo" />
+    <button v-if="active < all" @click="clear">清理</button>
+    <ul v-if="todos.length">
+      <li v-for="todo in todos" :key="todo.title">
+        <input type="checkbox" v-model="todo.done" />
+        <span :class="{ done: todo.done }">{{ todo.title }}</span>
+      </li>
+    </ul>
+    <div v-else>暂无数据</div>
+    <div>
+      全选<input type="checkbox" v-model="allDone" />
+      <span>{{ active }}/{{ all }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+
+let { title, todos, addTodo, clear, active, all, allDone } = useTodos();
+
+//一个组件有多段功能代码的话，可以把功能独立的模块封装成一个独立的函数
+//如果是可以公用的，那么可以单独提取成一个js文件放在utils里
+function useTodos() {
+  let title = ref("");
+  let todos = ref([{ title: "学习vue", done: false }]);
+
+  function addTodo() {
+    todos.value.push({
+      title: title.value,
+      done: false,
+    });
+    title.value = "";
+  }
+  function clear() {
+    todos.value = todos.value.filter((v) => !v.done);
+  }
+
+  let active = computed(() => {
+    return todos.value.filter((v) => !v.done).length;
+  });
+  let all = computed(() => todos.value.length);
+  let allDone = computed({
+    get: function () {
+      return active.value === 0;
+    },
+    set: function (val) {
+      todos.value.forEach((v) => (v.done = val));
+    },
+  });
+
+  return { title, todos, addTodo, clear, active, all, allDone };
+}
+</script>
+
+<style scoped>
+.done {
+}
+</style>
