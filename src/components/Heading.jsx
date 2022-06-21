a0679d1109a4ddf8b4b4c034ee2954986f8af162
tuyaqin1 <806149557@qq.com>
2022-06-21 22:39:32 +0800

    学习使用jsx

---
+++ src/components/Heading.jsx
@@ -0,0 +1,15 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    level: {
+      type: Number,
+      required: true
+    }
+  },
+  setup(props, { slots }) {
+    const tag = 'h' + props.level
+
+    return () => <tag>{ slots.default()}{ slots.header() }</tag>
+  }
+})
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>这是关于页面</h1>
+  <Heading :level="1">
+    <span>标题12</span>
+    <template v-slot:header> 
+      <span>1234</span>
+    </template>
+  </Heading>
 </template>
+
+<script setup>
+import Heading from '../components/Heading.jsx'
+</script>
+
+<style scoped>
+
+</style>
